e2ea654c7bc48e978a63724139e346f5ff8db810
Mustafa326 <58446336+Mustafa326@users.noreply.github.com>
2024-03-14 14:37:29 +0500

    Update views.py

--- Codes_primarely_leaflet/Bootstrap-HTML and JAVA/Bootstrap4 HTML codes/DjangoMapbox/MapDjango/views.py
+++ Codes_primarely_leaflet/Bootstrap-HTML and JAVA/Bootstrap4 HTML codes/DjangoMapbox/MapDjango/views.py
@@ -1,3 +1,5 @@
+import json
+from geojson import Feature, FeatureCollection
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views import View
@@ -10,37 +12,35 @@ from django.contrib.gis.db.models.aggregates import Collect
 from django.contrib.gis.db.models import Q
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from django.core.serializers import serialize
+from django.core.serializers.json import DjangoJSONEncoder
+from vectortiles.mixins import BaseVectorTileView
+from decimal import Decimal
 def index(request):
     return render(request, 'index.html')
-# Create your views here.
-def map_view(request):
-    # Fetch data from the database
-    data = FinalShapefile.objects.all()
+class DecimalEncoder(DjangoJSONEncoder):
+    def default(self, o):
+        if isinstance(o, Decimal):
+            return float(o)
+        return super(DecimalEncoder, self).default(o)
 
-    # Convert data to GeoJSON format
-    geojson_data = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "MultiPolygon",
-                    "coordinates": feature.geom.coords,
-                },
-                "properties": {
-                    "name": feature.adm2_en,
-                    "population_2010": feature.pop_2010,
-                    # Add other properties as needed
-                },
-            }
-            for feature in data
-        ],
-    }
+class FinalShapefileTileView(MVTView):
+    model = FinalShapefile
+    vector_tile_layer_name = "finalshapefile"
+    vector_tile_fields = ('shape_leng', 'shape_area', 'gid', 'pop_2010', 'pop_2015', 'pop_2020', 'w_d_2010', 's_d_2010', 'w_n_2010', 's_n2010', 'w_d_2011', 's_d_2011', 'w_n_2011', 's_n_2011', 'w_d_2012', 's_d_2012', 'w_n_2012', 's_n_2012', 'w_d_2013', 's_d_2013', 'w_n_2013', 's_n_2013', 'w_d_2014', 's_d_2014', 'w_n_2014', 's_n_2014', 'w_d_2015', 's_d_2015', 'w_n_2015', 's_n_2015', 'w_d_2016', 's_d_2016', 'w_n_2016', 's_n_2016', 'w_d_2017', 's_d_2017', 'w_n_2017', 's_n_2017', 'w_d_2018', 's_d_2018', 'w_n_2018', 's_n_2018', 'w_d_2019', 's_d_2019', 'w_n_2019', 's_n_2019', 'n_2010', 'n_2011', 'n_2012', 'n_2013', 'n_2014', 'n_2015', 'n_2016', 'n_2017', 'n_2018', 'n_2019', 'p_2010', 'p_2011', 'p_2012', 'p_2013', 'p_2014', 'p_2015', 'p_2016', 'p_2017', 'p_2018', 'p_2019', 'latitude', 'longitude', 'area_km_2', 'abspop_10', 'abspop_15', 'abspop_20', 'adm2_en'
+,)  # Include all fields
+    vector_tile_geom_name = "geom"
 
-    # Pass the GeoJSON data to the template
-    return render(
-        request,
-        "map_view.html",
-        {"geojson_data": geojson_data},
-    )
-# Create your views here.
+    def get_queryset(self):
+        return FinalShapefile.objects.all()
+    #def get_queryset(self):
+     #   return FinalShapefile.objects.all()
+
+    #def get(self, request, *args, **kwargs):
+    #    finalshapefiletiles = super().get(request, *args, **kwargs)
+    #    final_shapefile_objects = FinalShapefile.objects.all()
+    #    finalshapefiletiles["data"]=json.load(serialize("geojson",finalshapefiletiles))
+
+        # You can perform any additional data processing if needed
+
+    #    return finalshapefiletiles 
