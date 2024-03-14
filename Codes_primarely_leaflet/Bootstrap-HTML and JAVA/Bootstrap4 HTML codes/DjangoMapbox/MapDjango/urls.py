ba16d62e202f25ace0caee12e1461702e50cb048
Mustafa326 <58446336+Mustafa326@users.noreply.github.com>
2024-03-14 14:36:57 +0500

    Update urls.py

--- Codes_primarely_leaflet/Bootstrap-HTML and JAVA/Bootstrap4 HTML codes/DjangoMapbox/MapDjango/urls.py
+++ Codes_primarely_leaflet/Bootstrap-HTML and JAVA/Bootstrap4 HTML codes/DjangoMapbox/MapDjango/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import index
+from .views import FinalShapefileTileView
 
 urlpatterns = [
     path('', index, name='index'),
+    path('finalshapefiletiles/<int:z>/<int:x>/<int:y>', FinalShapefileTileView.as_view(), name="finalshapefiletiles"),
 ]
